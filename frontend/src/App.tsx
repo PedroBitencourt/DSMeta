83132cac65593c1c37742d61199cbb2dcbf5a9d3
PedroBitencourt <pedro.bitencourt42@gmail.com>
2022-09-06 18:21:35 -0300

    useState

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
-import Header from "./components/Header"
+import Header from "./components/Header";
 
-import SalesCard from "./components/SalesCard"
+import SalesCard from "./components/SalesCard";
 
 function App() {
   return(
